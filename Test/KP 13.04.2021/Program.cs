6b2ec675de7efb5c95cc185bb15db14fb1ae3305
alexandronischenko <alexandr.onischenko.2003@gmail.com>
2021-04-13 15:35:24 +0300

    Контрольная работа 13.04.2021
    Решил кр по информатике
    Контрольная 1
    
    1. Условие задачи: Как известно, слишком высокая стопка тарелок может развалиться. Следовательно, в реальной жизни, когда высота стопки превысила бы некоторое пороговое значение, мы начали бы складывать тарелки в новую стопку. Реализуйте структуру данных SetofStacks, имитирующую реальную ситуацию. Структура SetofStacks должна состоять из нескольких стеков, новый стек создается, как только предыдущий достигнет порогового значения. Методы SetofStacks.push() и SetofStacks.pop() должны вести себя так же, как при работе с одним стеком (то есть метод pop() должен возвращать те же значения, которые бы он возвращал при использовании одного большого стека). Реализуйте функцию popAt(int index), которая осуществляет операцию pop c заданным внутренним стеком.
    
    2. Пусть имеется список действительных чисел a1→a2→...→aп. Сформировать новый список b1→b2→...→bп такой же размерности по следующему правилу: элемент bk равен сумме элементов исходного списка с номерами от 1 до k.
    
    3. Имеется ряд словарей с пересекающимися ключами (значения - положительные числа).
    
    Напишите 2 функции, которые делают с массивом словарей следующие операции: 1-ая функция max_dct(*dicts) формирует новый словарь по правилу: Если в исходных словарях есть повторяющиеся ключи, выбираем среди их значений максимальное и присваиваем этому ключу (например, в словаре_1 есть ключ “а” со значением 5, и в словаре_2 есть ключ “а”, но со значением 9. Выбираем максимальное значение, т. е. 9, и присваиваем ключу “а” в уже новом словаре). Если ключ не повторяется, то он просто переносится со своим значением в новый словарь (например, ключ “с” встретился только у одного словаря, а у других его нет. Следовательно, переносим в новый словарь этот ключ вместе с его значением). Сформированный словарь возвращаем. 2-ая функция sum_dct(*dicts) суммирует значения повторяющихся ключей. Значения остальных ключей остаются исходными. (Проводятся операции по аналогу первой функции, но берутся не максимумы, а суммы значений одноименных ключей). Функция возвращает сформированный словарь.

---
+++ Test/KP 13.04.2021/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KP_13._04._2021
+{
+    public struct SetOfStack<T>
+    {
+        private int StackSize { get; }
+        private List<Stack<T>> List { get; set; }
+        private int CurrentIndex { get; set; }
+
+        public SetOfStack(int stackSize)
+        {
+            StackSize = stackSize;
+            CurrentIndex = -1;
+            List = new List<Stack<T>>();
+        }
+
+        public void Push(T item)
+        {
+            if (CurrentIndex < 0 || List[CurrentIndex].Count == StackSize)
+            {
+                CurrentIndex++;
+                List.Add(new Stack<T>(StackSize));
+            }
+
+            var currentStack = List[CurrentIndex];
+            currentStack.Push(item);
+        }
+
+        public T Pop()
+        {
+            if (CurrentIndex > -1)
+            {
+                if (List[CurrentIndex].Count == 0)
+                {
+                    CurrentIndex--;
+                }
+                return List[CurrentIndex].Pop();
+            }
+
+            return default;
+        }
+
+        public T PopAt(int index)
+        {
+            return List[index].Pop();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            TestStack();
+            
+            //2
+            //GetNewList();
+
+            //3
+            //TestFunctions();
+            Console.ReadKey();
+        }
+
+        private static void TestStack()
+        {
+            var stackSize = 2;
+            var stacks = new SetOfStack<int>(stackSize);
+        
+            stacks.Push(1);
+            stacks.Push(2);
+            stacks.Push(3);
+            stacks.Push(4);
+            stacks.Pop();
+            stacks.PopAt(1);
+            stacks.Push(3);
+
+
+        }
+
+        private static void TestFunctions()
+        {
+            List<Dictionary<string, int>> dictionaries = new List<Dictionary<string, int>>();
+            
+            var d1 = new Dictionary<string, int> { { "a", 1 }, { "b", 2 }, { "c", 3 } };
+
+            var d2 = new Dictionary<string, int> { { "a", 1 }, { "b", 2 }, { "d", 3 } };
+
+            var d3 = new Dictionary<string, int> { { "a", 2 }, { "b", 2 }, { "e", 3 } };
+
+            dictionaries.Add(d1);
+            dictionaries.Add(d2);
+            dictionaries.Add(d3);
+
+            var result = MaxDict(dictionaries);
+            Console.WriteLine(string.Join("\r\n", result.Select(x => $"key: {x.Key}, value: {x.Value}")));
+
+            result = SumDict(dictionaries);
+            Console.WriteLine(string.Join("\r\n", result.Select(x => $"key: {x.Key}, value: {x.Value}")));
+        }
+
+        private static Dictionary<string, int> MaxDict(List<Dictionary<string, int>> dictionaries)
+        {
+            var result = new Dictionary<string, int>();
+
+            result = dictionaries.SelectMany(dict => dict)
+                 .ToLookup(pair => pair.Key, pair => pair.Value)
+                 .ToDictionary(group => group.Key, group => group.Max());
+
+            return result;
+        }
+
+        private static Dictionary<string, int> SumDict(List<Dictionary<string, int>> dictionaries)
+        {
+            var result = new Dictionary<string, int>();
+
+            result = dictionaries.SelectMany(dict => dict)
+                 .ToLookup(pair => pair.Key, pair => pair.Value)
+                 .ToDictionary(group => group.Key, group => group.Sum());
+
+            return result;
+        }
+
+        private static void GetNewList()
+        {
+            List<double> list = new List<double> {1.25, 3.15, 5.31, 4.23, 9.23};
+            Console.WriteLine(string.Join("\t", list.Select(x => x.ToString())));
+            List<double> newList = GetNewList(list);
+            Console.WriteLine(string.Join("\t", newList.Select(x => x.ToString())));
+        }
+
+        private static List<double> GetNewList(List<double> list)
+        {
+            var result = new List<double>();
+            var temp = new List<double>();
+
+            for (var i = 0; i < list.Count; i++)
+            {
+                temp = list.GetRange(0, i + 1);
+                var sum = temp.Sum();
+                result.Add(sum);
+            }
+
+            return result;
+        }
+
+    }
+}
